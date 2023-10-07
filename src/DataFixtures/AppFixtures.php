6b18bcc8765e25686c501a3130673f432e5fe42f
CAMARA <88945535+camara92@users.noreply.github.com>
2023-10-07 17:44:42 +0200

    data fuxtures comments

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Blogpost;
 use App\Entity\Categorie;
+use App\Entity\Commentaire;
 use App\Entity\Peinture;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -28,45 +29,52 @@ class AppFixtures extends Fixture
             ->setPassword('daouda');
         $manager->persist($user);
 
-        for ($i=0; $i <= 15; $i++) {
+        for ($i = 0; $i <= 15; $i++) {
             $blogpost = new Blogpost();
             $blogpost->setTitre($faker->word(3, true))
                 ->setCreatedAt($faker->dateTimeBetween('-6 month', 'now'))
                 ->setContenu($faker->text(350))
                 ->setSlug($faker->slug())
-                ->setUser($user)
-                ;
+                ->setUser($user);
 
             $manager->persist($blogpost);
         }
-        for ($i=0; $i <= 6; $i++) {
+        for ($i = 0; $i <= 6; $i++) {
             $categorie = new Categorie();
             $categorie->setNom($faker->word(3, true))
                 ->setDescription($faker->words(10, true))
-                ->setSlug($faker->slug())
-                
-                ;
+                ->setSlug($faker->slug());
             // creation de peinture par categorie 
             $manager->persist($categorie);
-            for($p=0; $p<=3; $p++){
+            for ($p = 0; $p <= 3; $p++) {
                 $peinture = new Peinture();
-                $peinture-> setNom($faker->userName())
-                ->setLargeur($faker->randomFloat(2,20,60))
-                ->setHauteur($faker->randomFloat(2,20,60))
-                ->setEnVente($faker->randomElement([true, false]))
-                ->setDateRealisation($faker->dateTimeBetween('-6 month', 'now'))
-                ->setCreatedAt($faker->dateTimeBetween('-6 month', 'now'))
-                ->setDescription($faker->text())
-                ->setPortefolio($faker->randomElement([true, false]))
-                ->setSlug($faker->slug())
-                ->setFile("https://picsum.photos/200/300")
-                ->addCategorie($categorie)
-                ->setPrix($faker->randomFloat(2,100,9999))
-                ->setUser($user)
-                ;
-    
-                $manager->persist($peinture); 
+                $peinture->setNom($faker->userName())
+                    ->setLargeur($faker->randomFloat(2, 20, 60))
+                    ->setHauteur($faker->randomFloat(2, 20, 60))
+                    ->setEnVente($faker->randomElement([true, false]))
+                    ->setDateRealisation($faker->dateTimeBetween('-6 month', 'now'))
+                    ->setCreatedAt($faker->dateTimeBetween('-6 month', 'now'))
+                    ->setDescription($faker->text())
+                    ->setPortefolio($faker->randomElement([true, false]))
+                    ->setSlug($faker->slug())
+                    ->setFile("https://picsum.photos/200/300")
+                    ->addCategorie($categorie)
+                    ->setPrix($faker->randomFloat(2, 100, 9999))
+                    ->setUser($user);
+
+                $manager->persist($peinture);
             }
+        }
+
+        for ($i = 0; $i < 10; $i++) {
+            $commentaire = new Commentaire();
+            $commentaire->setAuteur($faker->firstName())
+                ->setEmail($faker->email())
+                ->setContenu($faker->text())
+                ->setCreatedAt($faker->dateTimeBetween('-6 month', 'now'))
+                ->setBlogpost($blogpost)
+                ->setPeinture($peinture);
+            $manager->persist($commentaire);
         }
 
 
